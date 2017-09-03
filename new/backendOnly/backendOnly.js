97799f1b2a56cc0d20579f6c18e1c55475b20a55
Devon Bull <dbull@live.com>
2017-09-02 23:55:37 -0500

    backend only now separated into files

--- new/backendOnly/backendOnly.js
+++ new/backendOnly/backendOnly.js
@@ -3,13 +3,16 @@ let path = require('path')
 
 let name = process.argv[3]
 
-let server = `let express = require('express')\nlet app = express()\nlet bodyParser = require('body-parser')\nconst routes = require('./routes')\nlet port = (process.env.PORT || 3000)\napp.use(bodyParser.json())\n\napp.use('/api/v1', routes)\n\napp.listen(port, () => {\n\tconsole.log('Listening at port 3000')\n})`
+let server = fs.readFileSync(path.resolve(__dirname, './files/server.js'), 'utf8')
 
 // files that dont change
-let gitignore = 'node_modules\n.DS_Store\n.env'
-let readme = '## bootstrapped with enzo'
-let routes = `const express = require('express')\nconst r = express.Router()\nmodule.exports = r`
-let spaNoSQLPck = `{\n\t"name": "${name}",\n\t"version": "1.0.0",\n\t"scripts": {\n\t\t"start": "nodemon server/server.js",\n\t\t"build": "webpack --watch"\n\t}\n}`
+
+let gitignore = fs.readFileSync(path.resolve(__dirname, '../filesToCopy/commonFiles/.gitignore'), 'utf8')
+let readme = fs.readFileSync(path.resolve(__dirname, '../filesToCopy/commonFiles/readme.md'), 'utf8')
+let routes = fs.readFileSync(path.resolve(__dirname, '../filesToCopy/commonFiles/routes.js'), 'utf8')
+
+
+let spaNoSQLPck = `{\n\t"name": "${name}",\n\t"version": "1.0.0",\n\t"scripts": {\n\t\t"start": "nodemon server/server.js"\n\t}\n}`
 
 let backendOnly = () => {
   fs.mkdirSync(`./${name}/server`)
