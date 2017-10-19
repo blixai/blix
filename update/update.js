89bc06492f76a032d20ff5908e7a46a5513e1b3e
Devon Bull <dbull@live.com>
2017-10-18 21:17:17 -0700

    update command in line with npm standards

--- update/update.js
+++ update/update.js
@@ -1,22 +1,7 @@
 let shell = require('shelljs')
-const execSync = require('child_process').execSync;
-
-let shouldUseYarn = () => {
-  try {
-    execSync('yarnpkg --version', { stdio: 'ignore' });
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
 
 let install = (packages) => {
-  let yarn = shouldUseYarn()
-  if (yarn) {
-    shell.exec(`yarn global add ${packages}`)
-  } else {
-    shell.exec(`npm install -g ${packages}`)
-  }
+  shell.exec(`npm update -S ${packages}`)
 }
 
 let update = () => {
