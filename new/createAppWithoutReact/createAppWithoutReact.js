dc90011a8a49b0a8404a4c39f299ad97b42612e0
Devon Bull <dbull@live.com>
2017-10-21 14:26:09 -0700

    embed enzo api and new page commands into rails style projects, fixed problem in BE only enzo projects

--- new/createAppWithoutReact/createAppWithoutReact.js
+++ new/createAppWithoutReact/createAppWithoutReact.js
@@ -9,7 +9,7 @@ let name = process.argv[3]
 let gitignore = fs.readFileSync(path.resolve(__dirname, '../filesToCopy/commonFiles/gitIgnore.js'), 'utf8')
 let readme = fs.readFileSync(path.resolve(__dirname, '../filesToCopy/commonFiles/readme.md'), 'utf8')
 let routes = fs.readFileSync(path.resolve(__dirname, '../filesToCopy/commonFiles/routes.js'), 'utf8')
-let spaNoSQLPck = `{\n\t"name": "${name}",\n\t"version": "1.0.0",\n\t"scripts": {\n\t\t"start": "gulp | nodemon --watch public server/server.js",\n\t\t"generate":"node enzo/index.js"\n\t}\n}`
+let spaNoSQLPck = `{\n\t"name": "${name}",\n\t"version": "1.0.0",\n\t"scripts": {\n\t\t"start": "gulp | nodemon --watch public server/server.js",\n\t\t"page":"node enzo/page.js"\n\t}\n}`
 
 let railsServer = fs.readFileSync(path.resolve(__dirname, './files/railsServer.js'), 'utf8')
 let railsHtmlFile = fs.readFileSync(path.resolve(__dirname, './files/railsHtmlFile.html'), 'utf8')
@@ -20,6 +20,11 @@ let gulpFile = fs.readFileSync(path.resolve(__dirname, './files/gulpFile.js'), '
 let indexHtml = `<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta charset="utf-8">\n\t\t<meta name="viewport" content="width=device-width, initial-scale=1">\n\t\t<title>Home</title>\n\t\t<link rel='stylesheet' type='text/css' href='main.css'/>\n\t</head>\n\t<body>\n\t\t<h1>Hello World</h1>\n\t\t<script src="index.js"></script>\n\t</body>\n</htlm>`
 let mainCss = `h1 {\n\tcolor: blue\n}`
 
+let enzoNewPage = fs.readFileSync(path.resolve(__dirname, './files/enzoNewPage.js'), 'utf8')
+let htmlPageTemplate = fs.readFileSync(path.resolve(__dirname, './templates/htmlPageTemplate.html'), 'utf8')
+let enzoAPI = fs.readFileSync(path.resolve(__dirname, './files/enzoAPI.js'), 'utf8')
+let enzoEndpointTemplate = fs.readFileSync(path.resolve(__dirname, './templates/enzoEndpointTemplate.js'), 'utf8')
+let enzoModelTemplate = fs.readFileSync(path.resolve(__dirname, './templates/enzoModelTemplate.js'), 'utf8')
 
 let railsApp = () => {
   fs.mkdirSync(`./${name}/src`)
@@ -62,12 +67,23 @@ let railsApp = () => {
   })
 
   //enzo files
-  let enzoIndex = fs.readFileSync(path.resolve(__dirname, './files/enzo.js'), 'utf8')
   fs.mkdirSync(`./${name}/enzo`)
-  fs.writeFile(`./${name}/enzo/index.js`, enzoIndex, (err) => {
+  fs.mkdirSync(`./${name}/enzo/templates`)
+  fs.writeFile(`./${name}/enzo/page.js`, enzoNewPage, (err) => {
     if (err) throw err 
   })
-  
+  fs.writeFile(`./${name}/enzo/templates/htmlPageTemplate.html`, htmlPageTemplate, (err) => {
+    if (err) console.error(err)
+  })
+  fs.writeFile(`./${name}/enzo/api.js`, enzoAPI, (err) => {
+    if (err) console.error(err) 
+  })
+  fs.writeFile(`./${name}/enzo/templates/enzoEndpointTemplate.js`, enzoEndpointTemplate, (err) => {
+    if (err) console.error(err)
+  })
+  fs.writeFile(`./${name}/enzo/templates/enzoModelTemplate.js`, enzoModelTemplate, (err) => {
+    if (err) console.error(err)
+  })
 }
 
 let createBasicApp = () => {
