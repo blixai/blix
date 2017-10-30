488e8a916187cf03b946d8f12ae60c2eb1ff6d40
Devon Bull <dbull@live.com>
2017-10-29 22:00:10 -0700

    add backend controller improvement

--- backend/files/enzoCreateAPI.js
+++ backend/files/enzoCreateAPI.js
@@ -6,14 +6,15 @@ if (name) {
   let endpoints = fs.readFileSync(path.resolve(__dirname, './templates/enzoEndpointTemplate.js'), 'utf8')
   endpoints = endpoints.replace(/Name/g, `${name}`)
   let controller = fs.readFileSync(path.resolve(__dirname, './templates/enzoControllerTemplate.js'), 'utf8')
-  controller = models.replace(/Name/g, `${name}`)
+  controller = controller.replace(/Name/g, `${name}`)
   fs.appendFile('./server/routes.js', endpoints, (err) => {
     if (err) throw err
     console.log('Created Routes:')
-    console.log(`GET: api/v1/${name}`)
-    console.log(`PUT: api/v1/${name}/:id`)
-    console.log(`DELETE: api/v1/${name}/:id `)
-    console.log(`POST: api/v1/${name} `)
+    console.log('')
+    console.log(`\tGET: api/v1/${name}`)
+    console.log(`\tPUT: api/v1/${name}/:id`)
+    console.log(`\tDELETE: api/v1/${name}/:id `)
+    console.log(`\tPOST: api/v1/${name} `)
   })
 
   fs.writeFile(`./server/controllers/${name}.js`, controller, (err) => {
