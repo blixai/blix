d7e34a12186964842dc03fdb5624c024df713575
Devon Bull <dbull@live.com>
2018-02-18 10:45:36 -0800

    refactored createReactSPA but encountering weird message

--- new/createReactSPA/createReactSPA.js
+++ new/createReactSPA/createReactSPA.js
@@ -1,87 +1,64 @@
-let fs = require('fs')
-let path = require('path')
-let name = process.argv[3]
+let fs      = require('fs')
+let path    = require('path')
+let name    = process.argv[3]
+let helpers = require('../../helpers')
 
+// helper function to load files 
+let loadFile = filePath => {
+  return fs.readFileSync(path.resolve(__dirname, filePath), 'utf8')
+}
 
-let gitignore = fs.readFileSync(path.resolve(__dirname, '../filesToCopy/commonFiles/gitIgnore.js'), 'utf8')
-let readme = fs.readFileSync(path.resolve(__dirname, '../filesToCopy/commonFiles/readme.md'), 'utf8')
-let routes = fs.readFileSync(path.resolve(__dirname, '../filesToCopy/commonFiles/routes.js'), 'utf8')
-let babel = fs.readFileSync(path.resolve(__dirname, '../filesToCopy/commonFiles/.babelrc'), 'utf8')
+let gitignore              = loadFile('../filesToCopy/commonFiles/gitIgnore.js')
+let readme                 = loadFile('../filesToCopy/commonFiles/readme.md')
+let routes                 = loadFile('../filesToCopy/commonFiles/routes.js')
+let babel                  = loadFile('../filesToCopy/commonFiles/.babelrc')
 
 // spa 
-let spaServer = fs.readFileSync(path.resolve(__dirname, './files/spaServer.js'), 'utf8')
-let spaWebpack = fs.readFileSync(path.resolve(__dirname, './files/webpack.config.js'), 'utf8')
-let prodWebpack = fs.readFileSync(path.resolve(__dirname, './files/webpack.prod.js'), 'utf8')
-let spaIndex = fs.readFileSync(path.resolve(__dirname, './files/spaIndex.js'), 'utf8')
-let spaReact = fs.readFileSync(path.resolve(__dirname, './files/spaReact.js'), 'utf8')
-let spaNoSQLPck = `{\n\t"name": "${name}",\n\t"version": "1.0.0",\n\t"scripts": {\n\t\t"start": "nodemon server/cluster.js",\n\t\t"build": "webpack --watch",\n\t\t"prod": "webpack --config webpack.prod.js",\n\t\t"react":"node enzo/react.js",\n\t\t"api": "node enzo/api.js"\n\t}\n}`
-let spaHtmlFile = fs.readFileSync(path.resolve(__dirname, './files/spaHtmlFile.html'), 'utf8')
-let spaNoBE = `{\n\t"name": "${name}",\n\t"version": "1.0.0",\n\t"scripts": {\n\t\t"start": "webpack-dev-server --output-public-path=/build/ --inline --hot --open",\n\t\t"react": "node enzo/react.js",\n\t\t"prod": "webpack --config webpack.prod.js"\n\t}\n}`
-let cluster = fs.readFileSync(path.resolve(__dirname, '../filesToCopy/cluster.js'), 'utf8')
-let postcss = fs.readFileSync(path.resolve(__dirname, './files/postcss.config.js'), 'utf8')
+let spaServer              = loadFile('./files/spaServer.js')
+let spaWebpack             = loadFile('./files/webpack.config.js')
+let prodWebpack            = loadFile('./files/webpack.prod.js')
+let spaIndex               = loadFile('./files/spaIndex.js')
+let spaReact               = loadFile('./files/spaReact.js')
+let spaNoSQLPck            = `{\n\t"name": "${name}",\n\t"version": "1.0.0",\n\t"scripts": {\n\t\t"start": "nodemon server/cluster.js",\n\t\t"build": "webpack --watch",\n\t\t"prod": "webpack --config webpack.prod.js",\n\t\t"react":"node enzo/react.js",\n\t\t"api": "node enzo/api.js"\n\t}\n}`
+let spaHtmlFile            = loadFile('./files/spaHtmlFile.html')
+let spaNoBE                = `{\n\t"name": "${name}",\n\t"version": "1.0.0",\n\t"scripts": {\n\t\t"start": "webpack-dev-server --output-public-path=/build/ --inline --hot --open",\n\t\t"react": "node enzo/react.js",\n\t\t"prod": "webpack --config webpack.prod.js"\n\t}\n}`
+let cluster                = loadFile('../filesToCopy/cluster.js')
+let postcss                = loadFile('./files/postcss.config.js')
 
 //enzo
-let stateless = fs.readFileSync(path.resolve(__dirname, './files/statelessComponent.js'), 'utf8')
-let stateful = fs.readFileSync(path.resolve(__dirname, './files/statefulComponent.js'), 'utf8')
-let enzoReact = fs.readFileSync(path.resolve(__dirname, './files/enzoReact.js'), 'utf8')
-let enzoEndpointTemplate = fs.readFileSync(path.resolve(__dirname, './templates/enzoEndpointTemplate.js'), 'utf8')
-let enzoControllerTemplate = fs.readFileSync(path.resolve(__dirname, './templates/enzoControllerTemplate.js'), 'utf8')
-let api = fs.readFileSync(path.resolve(__dirname, './files/enzoAPI.js'), 'utf8')
+let stateless              = loadFile('./files/statelessComponent.js')
+let stateful               = loadFile('./files/statefulComponent.js')
+let enzoReact              = loadFile('./files/enzoReact.js')
+let enzoEndpointTemplate   = loadFile('./templates/enzoEndpointTemplate.js')
+let enzoControllerTemplate = loadFile('./templates/enzoControllerTemplate.js')
+let api                    = loadFile('./files/enzoAPI.js')
 
 // this needs to be modified as the readline doesnt exist within this context
 let reactSPAWithoutBackend = () => {
   fs.mkdirSync(`./${name}/src`)
   fs.mkdirSync(`./${name}/build`)
-  fs.writeFile(`./${name}/index.html`, spaHtmlFile, (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/webpack.config.js`, spaWebpack, (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/webpack.prod.js`, prodWebpack, (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/postcss.config.js`, postcss, (err) => {
-    if (err) throw err 
-  })
-  fs.writeFile(`./${name}/src/index.js`, spaIndex, (err) => {
-    if (err) throw err
-  })
+  helpers.writeFile(`./${name}/index.html`, spaHtmlFile)
+  helpers.writeFile(`./${name}/webpack.config.js`, spaWebpack)
+  helpers.writeFile(`./${name}/webpack.prod.js`, prodWebpack)
+  helpers.writeFile(`./${name}/postcss.config.js`, postcss)
+  helpers.writeFile(`./${name}/src/index.js`, spaIndex)
   fs.mkdirSync(`./${name}/src/App`)
-  fs.writeFile(`./${name}/src/App/App.js`, spaReact, (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/src/App/App.css`, '', (err) => {
-    if (err) throw err
-  })
+  helpers.writeFile(`./${name}/src/App/App.js`, spaReact)
+  helpers.writeFile(`./${name}/src/App/App.css`, '')
 
   //enzo files 
   fs.mkdirSync(`./${name}/enzo`)
   fs.mkdirSync(`./${name}/enzo/templates`)
-  fs.writeFile(`./${name}/enzo/react.js`, enzoReact, (err) => {
-    if (err) console.error(err)
-  })
-  fs.writeFile(`./${name}/enzo/templates/statelessComponent.js`, stateless, (err) => {
-    if (err) console.error(err)
-  })
-  fs.writeFile(`./${name}/enzo/templates/statefulComponent.js`, stateful, (err) => {
-    if (err) console.error(err)
-  })
+  helpers.writeFile(`./${name}/enzo/react.js`, enzoReact)
+  helpers.writeFile(`./${name}/enzo/templates/statelessComponent.js`, stateless)
+  helpers.writeFile(`./${name}/enzo/templates/statefulComponent.js`, stateful)
   
-
   // other files
-  fs.writeFile(`./${name}/.gitignore`, gitignore, (err) => {
-    if (err) throw err
-  })
+  helpers.writeFile(`./${name}/.gitignore`, gitignore)
+  helpers.writeFile(`./${name}/README.md`, readme)
+  helpers.writeFile(`./${name}/.babelrc`, babel)
 
-  fs.writeFile(`./${name}/README.md`, readme, (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/.babelrc`, babel, (err) => {
-    if (err) throw err
-  })
   fs.writeFileSync(`./${name}/package.json`, spaNoBE)
-  // need to examine create react app more for hot reloading or use webpack dev server
 }
 
 let writeFilesWithSPAReact = () => {
@@ -89,82 +66,40 @@ let writeFilesWithSPAReact = () => {
   fs.mkdirSync(`./${name}/src`)
   fs.mkdirSync(`./${name}/build`)
   fs.mkdirSync(`./${name}/public`)
-  fs.writeFile(`./${name}/public/index.html`, spaHtmlFile, (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/webpack.config.js`, spaWebpack, (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/webpack.prod.js`, prodWebpack, (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/postcss.config.js`, postcss, (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/src/index.js`, spaIndex, (err) => {
-    if (err) throw err
-  })
+  helpers.writeFile(`./${name}/public/index.html`, spaHtmlFile)
+  helpers.writeFile(`./${name}/webpack.config.js`, spaWebpack)
+  helpers.writeFile(`./${name}/webpack.prod.js`, prodWebpack)
+  helpers.writeFile(`./${name}/postcss.config.js`, postcss)
+  helpers.writeFile(`./${name}/src/index.js`, spaIndex)
   fs.mkdirSync(`./${name}/src/App`)
-  fs.writeFile(`./${name}/src/App/App.js`, spaReact, (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/src/App/App.css`, '', (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/.babelrc`, babel, (err) => {
-    if (err) throw err
-  })
+  helpers.writeFile(`./${name}/src/App/App.js`, spaReact)
+  helpers.writeFile(`./${name}/src/App/App.css`, '')
+  helpers.writeFile(`./${name}/.babelrc`, babel)
+
   //backend
   fs.mkdirSync(`./${name}/server`)
   fs.mkdirSync(`./${name}/server/controllers`)
   fs.mkdirSync(`./${name}/server/models`)
   fs.mkdirSync(`./${name}/assets`)
-  
-  fs.writeFile(`./${name}/server/server.js`, spaServer, (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/server/cluster.js`, cluster, (err) => {
-    if (err) throw err 
-  })
-
-  fs.writeFile(`./${name}/server/routes.js`, routes, (err) => {
-    if (err) throw err
-  })
+  helpers.writeFile(`./${name}/server/server.js`, spaServer)
+  helpers.writeFile(`./${name}/server/cluster.js`, cluster)
+  helpers.writeFile(`./${name}/server/routes.js`, routes)
 
   //enzo files 
   fs.mkdirSync(`./${name}/enzo`)
   fs.mkdirSync(`./${name}/enzo/templates`)
-  fs.writeFile(`./${name}/enzo/react.js`, enzoReact, (err) => {
-    if (err) console.error(err)
-  })
-  fs.writeFile(`./${name}/enzo/templates/statelessComponent.js`, stateless, (err) => {
-    if (err) console.error(err)
-  })
-  fs.writeFile(`./${name}/enzo/templates/statefulComponent.js`, stateful, (err) => {
-    if (err) console.error(err)
-  })
-  fs.writeFile(`./${name}/enzo/api.js`, api, (err) => {
-    if (err) console.error(err)
-  })
-  fs.writeFile(`./${name}/enzo/templates/enzoControllerTemplate.js`, enzoControllerTemplate, (err) => {
-    if (err) console.error(err)
-  })
-  fs.writeFile(`./${name}/enzo/templates/enzoEndpointTemplate.js`, enzoEndpointTemplate, (err) => {
-    if (err) console.error(err)
-  })
-
+  fs.writeFile(`./${name}/enzo/react.js`, enzoReact)
+  fs.writeFile(`./${name}/enzo/templates/statelessComponent.js`, stateless)
+  fs.writeFile(`./${name}/enzo/templates/statefulComponent.js`, stateful)
+  helpers.writeFile(`./${name}/enzo/api.js`, api)
+  helpers.writeFile(`./${name}/enzo/templates/enzoControllerTemplate.js`, enzoControllerTemplate)
+  helpers.writeFile(`./${name}/enzo/templates/enzoEndpointTemplate.js`, enzoEndpointTemplate)
 
   //other files
-  fs.writeFile(`./${name}/.gitignore`, gitignore, (err) => {
-    if (err) throw err
-  })
-  fs.writeFile(`./${name}/.env`, '', (err) => {
-    if (err) throw err 
-  })
+  helpers.writeFile(`./${name}/.gitignore`, gitignore)
+  helpers.writeFile(`./${name}/.env`, '')
+  helpers.writeFile(`./${name}/README.md`, readme)
 
-  fs.writeFile(`./${name}/README.md`, readme, (err) => {
-    if (err) throw err
-  })
   fs.writeFileSync(`./${name}/package.json`, spaNoSQLPck)
 }
 
