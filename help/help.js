85a84f17bc152919db61c4c7c29d7b4b99628c7b
Devon Bull <dbull@bignerdranch.com>
2018-07-22 21:21:33 -0700

    removed chalk

--- help/help.js
+++ help/help.js
@@ -22,41 +22,41 @@ let runHelp = () => {
   prompt([commands]).then(answers => {
     switch (answers.help) {
       case 'backend':
-        log(chalk.cyanBright('Adds an Express.js server along with a MongoDB or Postgres database to your project after asking a series of questions'))
+        log('Adds an Express.js server along with a MongoDB or Postgres database to your project after asking a series of questions')
         break;
       case 'database':
-        log(chalk.cyanBright('Adds a MongoDB or Postgres Database to your project'))
+        log('Adds a MongoDB or Postgres Database to your project')
         break
       case 'help':
-        log(chalk.cyanBright('See a list of commands or learn more about a specific command'))
+        log('See a list of commands or learn more about a specific command')
         break
       case 'new':
-        log(chalk.cyanBright('Run blix new <projectName> and be asked a series of questions to start a project from scratch'))
+        log('Run blix new <projectName> and be asked a series of questions to start a project from scratch')
         break
       case 'redux':
-        log(chalk.cyanBright('Add redux to an existing react project. Be careful, it can cause file loss.'))
-        log(chalk.cyanBright('Works best with create-react-app projects or blix made react projects.'))
+        log('Add redux to an existing react project. Be careful, it can cause file loss.')
+        log('Works best with create-react-app projects or blix made react projects.')
         break
       case 'update':
-        log(chalk.cyanBright('updates blix from npm'))
+        log('updates blix from npm')
         break
       case 'add':
-        log(chalk.cyanBright('Choose from a list to add things like servers, databases, webpack, gulp, and redux to a project'))
+        log('Choose from a list to add things like servers, databases, webpack, gulp, and redux to a project')
         break
       case 'script':
-        log(chalk.cyanBright('Asks a series of questions to either add preconfigured blix scripts to a project or build your own'))
+        log('Asks a series of questions to either add preconfigured blix scripts to a project or build your own')
         break
       case 'webpack':
-        log(chalk.cyanBright('Add webpack to project. Asks for the entry directory and output folder. Configured for React, Sass, JS, CSS.'))
+        log('Add webpack to project. Asks for the entry directory and output folder. Configured for React, Sass, JS, CSS.')
         break
       case 'remove': 
-        log(chalk.cyanBright('Remove gulp or webpack from a project. Removes common dependencies, npm scripts, and files.'))
+        log('Remove gulp or webpack from a project. Removes common dependencies, npm scripts, and files.')
         break
       case 'gulp':
-        log(chalk.cyanBright('Add gulp to project. Asks for the entry directory and output folder. Configured for Sass, JS, CSS and Html.'))
+        log('Add gulp to project. Asks for the entry directory and output folder. Configured for Sass, JS, CSS and Html.')
         break 
       case 'version':
-        log(chalk.cyanBright('Displays the current version of blix'))
+        log('Displays the current version of blix')
         break
       default:
         noCommand()
