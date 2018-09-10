023e69fd109f35ad87f9f3cc9922bd6aa507614d
Devon Bull <dbull@apple.com>
2018-09-10 01:56:34 -0500

    updated help.js file

--- help/help.js
+++ help/help.js
@@ -9,9 +9,7 @@ let commands = {
   choices : [
     { name: 'new'     },
     { name: 'scripts' },
-    { name: 'add'     },
-    { name: 'help'    },
-    { name: 'version' }
+    { name: 'add'     }
   ]
 }
 
@@ -19,36 +17,18 @@ let runHelp = () => {
   console.clear()
   prompt([commands]).then(answers => {
     switch (answers.help) {
-      case 'backend':
-        log('Adds an Express.js server along with a MongoDB or Postgres database to your project after asking a series of questions')
-        break;
-      case 'database':
-        log('Adds a MongoDB or Postgres Database to your project')
-        break
-      case 'help':
-        log('See a list of commands or learn more about a specific command')
-        break
       case 'new':
         log('Run blix new <projectName> and be asked a series of questions to start a project from scratch')
         break
-      case 'redux':
-        log('Add redux to an existing react project. Be careful, it can cause file loss.')
-        log('Works best with create-react-app projects or blix made react projects.')
-        break
       case 'add':
         log('Choose from a list to add things like servers, databases, webpack, and redux to a project')
+        log('Try it: blix add')
         break
       case 'scripts':
         log('Asks a series of questions to either add preconfigured blix scripts to a project or build your own')
-        break
-      case 'webpack':
-        log('Add webpack to project. Asks for the entry directory and output folder. Configured for React, Sass, JS, CSS.')
-        break
-      case 'version':
-        log('Displays the current version of blix')
+        log('Try it: blix scripts')
         break
       default:
-        noCommand()
         break;
     }
   })
