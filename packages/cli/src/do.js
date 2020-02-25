06168f0cc31829f25848feb2a75bfbdbbf45877e
DBULL7 <devjbull@gmail.com>
2020-02-24 22:56:14 -0800

    version 1.6.3, prevent saved commands array from growing too large

--- packages/cli/src/do.js
+++ packages/cli/src/do.js
@@ -94,6 +94,9 @@ function _saveCommandToHistory(command) {
         const previousCommands = config.get(configField)
         if (Array.isArray(previousCommands)) {
             const filteredPreviousCommands = previousCommands.filter(cmd => cmd.trim() !== command)
+            if (filteredPreviousCommands.length >= 100) {
+                filteredPreviousCommands.pop()
+            }
             filteredPreviousCommands.unshift(command)
             config.set(configField, filteredPreviousCommands)
         }
